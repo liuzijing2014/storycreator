573c78a2bef9c118aa47df842b37be419ff26b5d
Zijing <Zijing@10.189.10.219>
2016-01-09 13:07:00 -0500

    mainarea

---
+++ StoryTelling/src/mainarea/MainWindow.java
@@ -0,0 +1,14 @@
+package mainarea;
+
+import javax.swing.JFrame;
+
+public class MainWindow extends JFrame{
+
+	private static final long serialVersionUID = 1L;
+	
+	MainWindow(){
+		super("StoryCreator");
+		
+	}
+
+}
