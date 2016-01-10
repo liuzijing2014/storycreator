d4c556ee52b6451586481aff44979ed80f189814
Shirley <Shirley@192.168.70.105>
2016-01-09 23:58:39 -0800

    chapter panel

--- StoryTelling/src/mainarea/CharpterPanel.java
+++ StoryTelling/src/mainarea/CharpterPanel.java
@@ -1,17 +1,27 @@
 package mainarea;
 
+import java.awt.BasicStroke;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Insets;
 import java.awt.Point;
+import java.awt.Polygon;
+import java.awt.Rectangle;
+import java.awt.RenderingHints;
 import java.awt.event.MouseEvent;
+import java.awt.geom.Area;
+import java.awt.geom.RoundRectangle2D;
 import java.util.Vector;
 
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 import javax.swing.SwingUtilities;
-import javax.swing.border.LineBorder;
+import javax.swing.border.AbstractBorder;
 import javax.swing.event.MouseInputAdapter;
 
 public class CharpterPanel extends JPanel {
@@ -31,14 +41,14 @@ public class CharpterPanel extends JPanel {
 		//this.parent = parent;
 		setLayout(new BorderLayout());
 		setSize(new Dimension(120, 80));
-		setBackground(Color.lightGray);
+		setBackground(Color.WHITE);
 		DragListener drag = new DragListener();
 		addMouseListener(drag);
 		addMouseMotionListener(drag);
-		setBorder(new LineBorder(colorList[colorIndex], 2));
-		text = new JLabel(display); 
+		setBorder(new RoundedBorder(colorList[colorIndex],2,16,0));
+		text = new JLabel(display, SwingConstants.CENTER); 
 		add(text);
-		setBounds(10, 10, 90, 60);
+		setBounds(10, 10, 120, 80);
 		
 		mEditArea = new EditArea(display);
 	}
@@ -79,7 +89,7 @@ public class CharpterPanel extends JPanel {
         	}
         	else if(SwingUtilities.isRightMouseButton(me)){
         		colorIndex = (colorIndex + 1) % 13;
-        		setBorder(new LineBorder(colorList[colorIndex], 2));
+        		setBorder(new RoundedBorder(colorList[colorIndex],2,16,0));
         	}
         }
 
@@ -91,5 +101,133 @@ public class CharpterPanel extends JPanel {
             component.setLocation(x, y);
             
         }
+    }
+}
+
+class RoundedBorder extends AbstractBorder {
+	private static final long serialVersionUID = -6969857696986122895L;
+	
+	private Color color;
+    private int thickness = 4;
+    private int radii = 8;
+    private int pointerSize = 7;
+    private Insets insets = null;
+    private BasicStroke stroke = null;
+    private int strokePad;
+    private int pointerPad = 4;
+    private boolean left = true;
+    RenderingHints hints;
+
+    RoundedBorder(
+            Color color) {
+        new RoundedBorder(color, 4, 8, 7);
+    }
+
+    RoundedBorder(
+            Color color, int thickness, int radii, int pointerSize) {
+        this.thickness = thickness;
+        this.radii = radii;
+        this.pointerSize = pointerSize;
+        this.color = color;
+
+        stroke = new BasicStroke(thickness);
+        strokePad = thickness / 2;
+
+        hints = new RenderingHints(
+                RenderingHints.KEY_ANTIALIASING,
+                RenderingHints.VALUE_ANTIALIAS_ON);
+
+        int pad = radii + strokePad;
+        int bottomPad = pad + pointerSize + strokePad;
+        insets = new Insets(pad, pad, bottomPad, pad);
+    }
+
+    RoundedBorder(
+            Color color, int thickness, int radii, int pointerSize, boolean left) {
+        this(color, thickness, radii, pointerSize);
+        this.left = left;
+    }
+
+    @Override
+    public Insets getBorderInsets(Component c) {
+        return insets;
+    }
+
+    @Override
+    public Insets getBorderInsets(Component c, Insets insets) {
+        return getBorderInsets(c);
+    }
+
+    @Override
+    public void paintBorder(
+            Component c,
+            Graphics g,
+            int x, int y,
+            int width, int height) {
+
+        Graphics2D g2 = (Graphics2D) g;
+
+        int bottomLineY = height - thickness - pointerSize;
+
+        RoundRectangle2D.Double bubble = new RoundRectangle2D.Double(
+                0 + strokePad,
+                0 + strokePad,
+                width - thickness,
+                bottomLineY,
+                radii,
+                radii);
+
+        Polygon pointer = new Polygon();
+
+        if (left) {
+            // left point
+            pointer.addPoint(
+                    strokePad + radii + pointerPad,
+                    bottomLineY);
+            // right point
+            pointer.addPoint(
+                    strokePad + radii + pointerPad + pointerSize,
+                    bottomLineY);
+            // bottom point
+            pointer.addPoint(
+                    strokePad + radii + pointerPad + (pointerSize / 2),
+                    height - strokePad);
+        } else {
+            // left point
+            pointer.addPoint(
+                    width - (strokePad + radii + pointerPad),
+                    bottomLineY);
+            // right point
+            pointer.addPoint(
+                    width - (strokePad + radii + pointerPad + pointerSize),
+                    bottomLineY);
+            // bottom point
+            pointer.addPoint(
+                    width - (strokePad + radii + pointerPad + (pointerSize / 2)),
+                    height - strokePad);
+        }
+
+        Area area = new Area(bubble);
+        area.add(new Area(pointer));
+
+        g2.setRenderingHints(hints);
+
+        // Paint the BG color of the parent, everywhere outside the clip
+        // of the text bubble.
+        Component parent  = c.getParent();
+        if (parent!=null) {
+            Color bg = parent.getBackground();
+            Rectangle rect = new Rectangle(0,0,width, height);
+            Area borderRegion = new Area(rect);
+            borderRegion.subtract(area);
+            g2.setClip(borderRegion);
+            g2.setColor(bg);
+            g2.fillRect(0, 0, width, height);
+            g2.setClip(null);
+        }
+
+        g2.setColor(color);
+        g2.setStroke(stroke);
+        g2.draw(area);
     }
 }
